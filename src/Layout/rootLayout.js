46faa088d381d15b4ad16a6974e96ad4a3cc3e7b
chinapicke <chinapickering@hotmail.co.uk>
2023-05-24 22:58:32 +0700

    style changes to navbar

--- src/Layout/rootLayout.js
+++ src/Layout/rootLayout.js
@@ -13,34 +13,45 @@ export default function RootLayout() {
     }
     return (
         <>
-            <div>
-                <div>
+            <div className='flex justify-between'>
+                <div className='py-2'>
                     ShopIt
                 </div>
 
-                <nav className='largeMenu hidden justify-between items-center w-full lg:flex lg:w-auto lg:order-1 '>
-                    <ul className='navOptions flex flex-col mt-4'>
-                        <li className='block py-2 pl-3 pr-4'>
-                            <NavLink to='/'>Home</NavLink>
-                        </li>
-                        <li block py-2 pl-3 pr-4>
-                            <NavLink to='/about'>About</NavLink>
-                        </li>
-                        <li>
-                            <NavLink to='/shop'>Shop</NavLink>
-                        </li>
-                        <li>
-                            <NavLink to='/cart'><FontAwesomeIcon icon={faMagnifyingGlass} /></NavLink>
-                        </li>
-                        <li>
-                            <NavLink to='/cart'><FontAwesomeIcon icon={faHeart} /> </NavLink>
-                        </li>
-                        <li>
-                            <NavLink to='/cart'><FontAwesomeIcon icon={faCartShopping} /></NavLink>
-                        </li>
+                <nav className='largeMenuCentre hidden justify-center w-full inline-block lg:flex lg:w-auto lg:order-1 '>
+                    <ul className='navOptions flex'>
+                        <div className='flex'>
+                            <li className='block py-2 pl-3 pr-4'>
+                                <NavLink to='/'>Home</NavLink>
+                            </li>
+                            <li className='block py-2 pl-3 pr-4'>
+                                <NavLink to='/about'>About</NavLink>
+                            </li>
+                            <li className='block py-2 pl-3 pr-4'>
+                                <NavLink to='/shop'>Shop</NavLink>
+                            </li>
+                        </div>
                     </ul>
                 </nav>
-                <div className='mobileBarIcon lg:hidden md:block sm:block pt-4' onClick={openDropdown}>
+                <nav className='hidden justify-end w-full lg:flex lg:w-auto lg:order-1' >
+                    <ul>
+                    <div className='flex pl-5'>
+                        <li className='block py-3 pl-3 pr-4'>
+                            <NavLink to='/cart'><FontAwesomeIcon icon={faMagnifyingGlass} /></NavLink>
+                        </li>
+                        <li className='block py-3 pl-3 pr-4'>
+                            <NavLink to='/cart'><FontAwesomeIcon icon={faHeart} /> </NavLink>
+                        </li>
+                        <li className='block py-3 pl-3 pr-4'>
+                            <NavLink to='/cart'><FontAwesomeIcon icon={faCartShopping} /></NavLink>
+                        </li>
+                    </div>
+                    </ul>
+                </nav>
+
+
+
+                <div className='mobileBarIcon lg:hidden md:block sm:block pt-2 pr-3' onClick={openDropdown}>
                     <FontAwesomeIcon icon={faBars} size="xl" />
                 </div>
                 {mobileOpen ?
