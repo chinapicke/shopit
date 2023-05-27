3c3026795ac2756bd560b9edfb0d23f211e137e7
chinapicke <chinapickering@hotmail.co.uk>
2023-05-27 09:41:22 +0700

    Counter working and using axios instead of fetch

--- src/Pages/Shop.js
+++ src/Pages/Shop.js
@@ -1,73 +1,104 @@
 import React from 'react'
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+import axios from 'axios'
 
 function Shop() {
 
-  const [product, setProduct] = useState([])
+  const [products, setProducts] = useState([])
+  // const [items, setItems] = useState([])
+  const [search, setSearch] = useState('')
 
   const decrementCount = (index) => {
-    setProduct((prevState) => {
-      const updatedProducts = [...prevState];
+    setProducts((prevState) => {
+      const updatedProducts = prevState.map(e => ({ ...e }));
       // create min for the count, can not go less than 0
-      const newCount = Math.max(0, updatedProducts[index].count - 1)
-      updatedProducts[index].count = newCount;
+      // math.max() returns the value that is the smallest
+      const newCountdown = Math.max(0, updatedProducts[index].count - 1)
+      updatedProducts[index].count = newCountdown;
       return updatedProducts
     });
-
   }
+
   const incrementCount = (index) => {
-    setProduct((prevState)=>{
-    const updatedProducts = [...prevState];
-    // create max for the count, can not go more than 10
-    const newCount = Math.min(10, updatedProducts[index].count + 1)
-    updatedProducts[index].count = newCount;
-    return updatedProducts
+    setProducts((prevState) => {
+      // const updatedProducts = [...prevState];
+      const updatedProducts = prevState.map(e => ({ ...e }));
+      // create max for the count, can not go more than 10
+      const newCount = Math.min(10, updatedProducts[index].count + 1)
+      updatedProducts[index].count = newCount;
+      // console.log('This is the count'+updatedProducts[index].count)
+      return updatedProducts;
+    });
   }
-    )};
 
-
+  useEffect(() => {
+    callAPI()
+  }, [])
 
   const url = 'http://makeup-api.herokuapp.com/api/v1/products.json?brand=maybelline'
 
-  async function callAPI() {
+  const callAPI = async () => {
     try {
-      const request = await fetch(url);
-      if (request.status === 200) {
+      const res = await axios.get(url)
+      if(res.status===200){
         console.log('Success!');
-        const response = await request.json();
-        // this spreads the items from the response call and adds a count of 1 to each card/image
-        const productsWithCount = response.map((item) => ({
-          ...item, count:1
-          
+        const productsWithCount = res.data.map((item) => ({
+          ...item, count: 1
         }));
-        setProduct(productsWithCount);
-      } else {
-        console.log(`Server error: ${request.status}`);
+        setProducts(productsWithCount);
       }
+       else {
+          console.log(`Server error: ${res.status}`);
+        }
     } catch (err) {
       console.log(`Fetch error: ${err}`);
     }
   }
+  // const callAPI = async () => {
+  //   try {
+  //     const data = await axios.get(url);
+  //     if (data.status === 200) {
+  //       console.log('Success!');
+  //       // this spreads the items from the response call and adds a count of 1 to each card/image
+  //       setProducts(data.map(e => ({...e, count:1})));
+  //     }
+  //     else{
+  //       console.log(`Server error: ${data.status}`)
+  //     }
+  //   }
+  //   catch(err){
+  //         console.log(`Axios error: ${err}`);
+  //       }
+
+  //   }
+
+
 
   return (
     <div>
+      <input
+        type="text"
+        placeholder="Search here"
+        onChange={(e) => setSearch(e.target.value)}
+        value={search} />
+      {/* <button onClick={searchButton}>Submit </button>  */}
       <button onClick={callAPI}>Click to get API</button>
       <div className='shopCards grid grid-cols-5'>
         {
-          product.map((item, index) => {
+          products.map((item, index) => {
             return (
-              <div>
+              <div key={item.id}>
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
                   <path stroke-linecap="round" stroke-linejoin="round" d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12z" />
                 </svg>
-                <img src={item.api_featured_image} key={item.id}></img>
+                <img src={item.api_featured_image} ></img>
                 <div className='addToCart'>
                   <div className='productQuantity'>
-                    <button onClick={()=>incrementCount(index)} value={item.id}>
+                    <button onClick={() => incrementCount(index)} value={item.id}>
                       +
                     </button>
                     <p>{item.count}</p>
-                    <button onClick={()=>decrementCount(index)}>
+                    <button onClick={() => decrementCount(index)}>
                       -
                     </button>
                   </div>
