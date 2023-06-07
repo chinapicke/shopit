fc0474ff8cace59faaf903e096761698ddd81356
chinapicke <chinapickering@hotmail.co.uk>
2023-06-07 14:15:36 +0700

    conditional rendering of loading and no products

--- src/Pages/Shop.js
+++ src/Pages/Shop.js
@@ -14,6 +14,7 @@ function Shop() {
   const [likedIndex, setLikedIndex] = useState([])
 
 
+
   // Adding quantity of product cards //////////////////////////////////////////
   // const decrementCount = (index) => {
   //   // setProducts((prevState) => {
@@ -69,11 +70,6 @@ function Shop() {
     }
   }
 
-  useEffect(() => {
-    callAPI()
-  }, [])
-
-
   // call when user filters by brand
   const getProductsByBrand = async (brandName) => {
     setIsLoading(true)
@@ -85,10 +81,6 @@ function Shop() {
         }
       });
       console.log(res.data)
-      if (res.data.length === 0) {
-        alert('There are no products')
-      }
-      else {
         if (res.status === 200) {
           console.log('Success!');
           const productsWithQuantity = res.data.map((item) => ({
@@ -101,7 +93,6 @@ function Shop() {
           console.log(`Server error: ${res.status}`);
         }
       }
-    }
     catch (err) {
       console.log(`Fetch error: ${err}`);
     }
@@ -161,6 +152,11 @@ function Shop() {
       console.log(`Fetch error: ${err}`);
     }
   }
+
+  useEffect(() => {
+    callAPI()
+  }, [])
+
   ////////////////////////////////////////////////////////////////////////
 
   // useContext for the add to cart 
@@ -191,63 +187,61 @@ function Shop() {
       <OptionButtons onButton={selectAProduct} />
       <div className='shopCards grid grid-cols-2'>
         {/* condition that if user selects submit btn, then to show the cards with the searchProducts API if not then to show cards on start page */}
+        {!isLoading ? <>
+          {products.length ?
+              products.map((item, index) => {
+                item.quantity = 1
+                return (
+                  <div key={item.id}>
+                    <button value={item.brand + item.product_type} onClick={() => { changeIcon(index); saveDispatch({ type: 'SAVE', saveIt: item }) }}>
+                      {likedIndex[index] ?
+                        (
+                          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-6 h-6">
+                            <path d="M11.645 20.91l-.007-.003-.022-.012a15.247 15.247 0 01-.383-.218 25.18 25.18 0 01-4.244-3.17C4.688 15.36 2.25 12.174 2.25 8.25 2.25 5.322 4.714 3 7.688 3A5.5 5.5 0 0112 5.052 5.5 5.5 0 0116.313 3c2.973 0 5.437 2.322 5.437 5.25 0 3.925-2.438 7.111-4.739 9.256a25.175 25.175 0 01-4.244 3.17 15.247 15.247 0 01-.383.219l-.022.012-.007.004-.003.001a.752.752 0 01-.704 0l-.003-.001z" />
+                          </svg>
+                        )
+                        : (
+                          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6" >
+                            <path strokeLinecap="round" strokeLinejoin="round" d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12z" />
+                          </svg>
+                        )
+                      }
 
-        {!isLoading ?
-          (products.map((item, index) => {
-            item.quantity = 1
-            return (
-              <>
-                <div key={item.id}>
-                  <button value={item.brand + item.product_type} onClick={() => { changeIcon(index); saveDispatch({ type: 'SAVE', saveIt: item }) }}>
-                    {likedIndex[index] ?
-                      (
-                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-6 h-6">
-                          <path d="M11.645 20.91l-.007-.003-.022-.012a15.247 15.247 0 01-.383-.218 25.18 25.18 0 01-4.244-3.17C4.688 15.36 2.25 12.174 2.25 8.25 2.25 5.322 4.714 3 7.688 3A5.5 5.5 0 0112 5.052 5.5 5.5 0 0116.313 3c2.973 0 5.437 2.322 5.437 5.25 0 3.925-2.438 7.111-4.739 9.256a25.175 25.175 0 01-4.244 3.17 15.247 15.247 0 01-.383.219l-.022.012-.007.004-.003.001a.752.752 0 01-.704 0l-.003-.001z" />
-                        </svg>
-                      )
-                      : (
-                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6" >
-                          <path strokeLinecap="round" strokeLinejoin="round" d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12z" />
-                        </svg>
-                      )
-                    }
-
-                  </button>
-                  <img src={item.api_featured_image} alt={item.brand + item.product_type}></img>
-                  {/* <p>{item.product_colours}</p> */}
-                  {/* To display the brand name with as sentence case */}
-                  <p>{item?.brand ? item.brand.charAt(0).toUpperCase() + item.brand.slice(1).toLowerCase() : item.brand} {item?.name ? item.name.charAt(0).toUpperCase() + item.name.slice(1).toLowerCase() : item.name}</p>
-                  <p>{item?.product_type.charAt(0).toUpperCase() + item.product_type.slice(1).toLowerCase()}</p>
-                  <p>£{
-                    // Condition if the price is 0, give it a default of '8.5'
-                    (item.price === '0.0')
-                      ? '8.5'
-                      : item.price
-                  }</p>
-                  <div className='addToCart'>
-                    {/* <div className='productQuantity'>
-                  <button
-                  onClick={()=>item.quantity+1}>
-                 onClick={() => incrementCount(index)}  
-                    +
-                  </button>
-                  <p>{item.quantity}</p>
-                  <button>
-                  onClick={() => decrementCount(index)} value={item.id}>
-                    -
-                  </button>
-                </div> */}
-                    <button onClick={() => dispatch({ type: 'ADD', payload: item })}>
-                      Add to cart
                     </button>
+                    <img src={item.api_featured_image} alt={item.brand + item.product_type}></img>
+                    {/* <p>{item.product_colours}</p> */}
+                    {/* To display the brand name with as sentence case */}
+                    <p>{item?.brand ? item.brand.charAt(0).toUpperCase() + item.brand.slice(1).toLowerCase() : item.brand} {item?.name ? item.name.charAt(0).toUpperCase() + item.name.slice(1).toLowerCase() : item.name}</p>
+                    <p>{item?.product_type.charAt(0).toUpperCase() + item.product_type.slice(1).toLowerCase()}</p>
+                    <p>£{
+                      // Condition if the price is 0, give it a default of '8.5'
+                      (item.price === '0.0')
+                        ? '8.5'
+                        : item.price
+                    }</p>
+                    <div className='addToCart'>
+                      <div className='productQuantity'>
+                        <button
+                          onClick={() => item.quantity + 1}>
+                          {/* onClick={() => incrementCount(index)}   */}
+                          +
+                        </button>
+                        <p>{item.quantity}</p>
+                        <button>
+                          {/* onClick={() => decrementCount(index)} value={item.id}> */}
+                          -
+                        </button>
+                      </div>
+                      <button onClick={() => dispatch({ type: 'ADD', payload: item })}>
+                        Add to cart
+                      </button>
+                    </div>
                   </div>
-                </div>
-              </>
-            )
-          }
-          )
-          ) : ('Loading...')
-        }
+                )
+              })
+             : <h1>No results found</h1>}
+        </> :
+          <h1>Loading...</h1>}
       </div>
     </div >
   )
