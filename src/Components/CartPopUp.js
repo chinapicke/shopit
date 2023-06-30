6849b2e43144c5be3914529bd3e3d622fb0c4141
chinapicke <chinapickering@hotmail.co.uk>
2023-07-01 00:07:29 +0800

    no style- cart drawer when click cart in header

---
+++ src/Components/CartPopUp.js
@@ -0,0 +1,48 @@
+import { AppContext } from '../Context/Context';
+import { useContext } from 'react';
+
+function CartPopUp() {
+    const CartState = useContext(AppContext);
+  const state = CartState.state;
+  const dispatch = CartState.dispatch;
+
+
+  const total = state.reduce((accumulator, currentValue) => {
+    return accumulator + currentValue.price * currentValue.quantity
+  }
+    , 0)
+  return (
+    <div>
+      {state.length === 0 ?
+        (<h1>Your cart is empty </h1>) :
+
+
+        state.map((item, index) => {
+          return (
+            <div className='addCard' key={index}>
+              <img src={item.api_featured_image} alt={item.brand + item.product_type}></img>
+              {/* To display the brand name with as sentence case */}
+              <p>{item?.brand ? item.brand.charAt(0).toUpperCase() + item.brand.slice(1).toLowerCase() : item.brand} {item?.name ? item.name.charAt(0).toUpperCase() + item.name.slice(1).toLowerCase() : item.name}</p>
+              <p>£{(item?.price === '0.0')
+                ? item.price = '8.50'
+                : Number(item.price).toFixed(2)}</p>
+              <p>{item.quantity}</p>
+              <button onClick={() => { console.log(item.id); dispatch({ type: "REMOVE", payload: item }) }}>
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+                  <path strokeLinecap="round" strokeLinejoin="round" d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0" />
+                </svg>
+              </button>
+            </div>
+          )
+        })
+      }
+      {state.length > 0 && (
+        < h1 > Total:£{total.toFixed(2)}</h1>
+      )}
+    </div >
+
+  )
+}
+
+
+export default CartPopUp
--- src/Layout/rootLayout.js
+++ src/Layout/rootLayout.js
@@ -6,9 +6,12 @@ import { faBars } from '@fortawesome/free-solid-svg-icons'
 import { useState } from 'react';
 import { useContext } from 'react';
 import { AppContext } from '../Context/Context';
+import CartPopUp from '../Components/CartPopUp';
 
 export default function RootLayout() {
     const [mobileOpen, setMobileOpen] = useState(false);
+    const [cartDrawer, setCartDrawer] = useState(false);
+
 
     const CartState = useContext(AppContext);
     const state = CartState.state
@@ -25,6 +28,11 @@ export default function RootLayout() {
     const openDropdown = () => {
         setMobileOpen(current => !current);
     }
+    const openDrawer = () => {
+        setCartDrawer(current => !current);
+    }
+
+ 
 
     return (
         <>
@@ -65,10 +73,14 @@ export default function RootLayout() {
                                 </NavLink>
                             </li>
                             <li className='block py-3 pl-3 pr-4'>
-                                <NavLink to='/cart'><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6">
+                                <NavLink to='/cart'>
+                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6" onClick={openDrawer}>
                                     <path strokeLinecap="round" strokeLinejoin="round" d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 00-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 00-16.536-1.84M7.5 14.25L5.106 5.272M6 20.25a.75.75 0 11-1.5 0 .75.75 0 011.5 0zm12.75 0a.75.75 0 11-1.5 0 .75.75 0 011.5 0z" />
                                 </svg>
                                 <span>{totalCartQuantity<=0?null:totalCartQuantity }</span>
+                                {cartDrawer ?
+                                <CartPopUp /> : null
+                                }
                                 </NavLink>
                             </li>
                         </div>
