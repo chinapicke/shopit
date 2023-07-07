87961174417f5550db312e527caf3fbf198c8ea4
chinapicke <chinapickering@hotmail.co.uk>
2023-07-07 22:05:50 +0800

    text added to about page

--- src/Pages/About.js
+++ src/Pages/About.js
@@ -2,7 +2,21 @@ import React from 'react'
 
 function About() {
   return (
-    <div>This is the about page</div>
+    <div className='aboutPage'>
+      <h1>THINK BEAUTY, THINK US.</h1>
+
+      <p>With practically 850+ curated, very much valued and 100% certified brands,
+         ShopIt prides itself on offering a far-reaching choice of cosmetics for everyone. 
+         We expect to if you don’t mind heading off to the most distant corners of the nation to contact you!</p>
+
+      <h1>Adore It. Offer It. Live It.</h1>
+      <p>Today ShopIt transports over the length and expansiveness of the nation to pretty much every postal 
+        division utilizing the administrations of driving and dependable dispatch organizations.</p>
+
+      <h1>Our promise</h1>
+      <p>We are focused on curating the best of characteristic beauty without trading off on proficiency and guaranteeing that every one of our items is cold-bloodedness-free.</p>
+      
+    </div>
     
   )
 }
