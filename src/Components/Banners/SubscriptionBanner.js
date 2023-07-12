25fbb2188670ff115fadc8a740df21e0d42022f3
chinapicke <chinapickering@hotmail.co.uk>
2023-07-12 15:47:18 +0800

    responsive subscription banner

--- src/Components/Banners/SubscriptionBanner.js
+++ src/Components/Banners/SubscriptionBanner.js
@@ -4,21 +4,21 @@ import YellowDot from '../../Assets/Images/Backgrounds/yellowdot.png'
 
 function SubscriptionBanner() {
     return (
-        <div className='subscriptionBanner py-6 static'>
-            <img className='present w-42 h-32 z-10 absolute ml-4' src={Present} alt='present'></img>
-            <div className='subscriptionColoured flex py-3 px-1'>
-                <img className='background relative h-2/3 ' src={YellowDot} alt='subscriptionBackground' />
-                <div className='subscriptionColouredText absolute ml-16 mt-3 text-right w-2/3'>
-                <h1 className='w-2/3 ml-16'>We have something for you!</h1>
-                <p className='w-2/3 ml-16'>Information about the subscription</p>
-                <form className="formSubscription max-w-sm ml-5">
-                    <div className="flex items-center border-b border-black py-2">
-                        <input className="appearance-none bg-transparent border-none w-full text-black py-1 pl-10 leading-tight focus:outline-none placeholder-black  md:mr-3" type="email" placeholder="Your email" />
+        <div className='subscriptionBanner mx-4 py-3 lg:ml-10 '>
+            <div className='presentBackground relative flex justify-center ml-6 '>
+                <img className='background py-3 ' src={YellowDot} alt='subscriptionBackground' />
+                <img className='present' src={Present} alt='present'></img>
+                <div className='subscriptionColouredText absolute text-right '>
+                    <h1 >We have something for you!</h1>
+                    <p className='pSubscription'>Join our weekly email for discount and offers!</p>
+                    <form className="formSubscription max-w-sm ml-5 pt-1">
+                        <div className="flex items-center border-b border-black pl-8 xl:pl-16">
+                            <input className="appearance-none bg-transparent border-none w-full text-black py-1  leading-tight focus:outline-none placeholder-black " type="email" placeholder="Your email" />
+                        </div>
                         <button className="subscribeBtn bg-black hover:bg-yellow-300 border-black hover:border-black border-4 text-white py-1 px-1 rounded-full" type="button">
-                            Subscribe
+                                Subscribe
                         </button>
-                    </div>
-                </form>
+                    </form>
                 </div>
             </div>
         </div>
