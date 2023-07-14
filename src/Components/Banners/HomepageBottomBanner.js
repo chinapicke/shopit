fb4e39284736d819b041f40699cbbb5cdc1841b2
chinapicke <chinapickering@hotmail.co.uk>
2023-07-14 22:01:30 +0800

    changed formatting of most of homepage

--- src/Components/Banners/HomepageBottomBanner.js
+++ src/Components/Banners/HomepageBottomBanner.js
@@ -12,16 +12,16 @@ function HomepageBottomBanner() {
 
     <div className='homepageBottomBanner grid grid-cols-2 mx-0 md:mx-20 my-3'>
       <div>
-                <h1 className='w-44 py-1 px-3 text-lg'>Be confident,
+          <h1 className='beParagraph w-44 py-1 px-3 text-lg md:text-2xl lg:text-4xl'>Be confident,
             be beautiful,
             be you.
         </h1>
-        <button onClick={BtnDirectShop} className='productsSeeBtn text-xs bg-black text-white px-2 my-2 mb-0 md:my-6 ml-2 text-sm py-4 px-6 rounded-full mr-auto ' type="button">See more</button>
+        <button onClick={BtnDirectShop} className='productsSeeBtn text-xs bg-black text-white px-2 my-2 mb-0 md:my-6 ml-2 text-sm py-4 px-6 rounded-full mr-auto lg:text-lg' type="button">See more</button>
       </div>
       <div>
-      <p className='flex justify-end pb-1 mr-5 text-lg'>£89.99</p>
-          <p className='perSet flex justify-end mr-5 text-xs md:mr-10 lg:mr-24 xl:mr-32'>per set</p>
-        <div className='flex justify-end'>
+      <p className='bannerPrice flex justify-center pb-1 mr-10 mt-2 text-lg  '>£89.99</p>
+          <p className='perSet flex justify-center mr-5 text-xs md:mr-10 lg:mr-24 xl:mr-32 '>per set</p>
+        <div className='flex '>
         <img className='cliniqueSet ' src={CliniqueSet} alt='cliniqueSet'></img>
       </div>
 
--- src/Pages/Homepage.js
+++ src/Pages/Homepage.js
@@ -30,25 +30,26 @@ function Homepage() {
       </div>
         <TopPicks />
       </div>
-      <div className='homepagePictures gap-4 h-5/6'>
-        <div className='leftMidPicture h-full md:h-5/6 xl:h-5/6'>
-          <h3 className='pl-1 md:text-2xl md:py-2 md:pl-3 lg:text-3xl xl:text-5xl'>Fast and ecological solutions for you.</h3>
-          <button onClick={BtnDirectShop} className='productsSeeBtn text-xs bg-black text-white px-2 my-2 md:my-6 ml-2 text-sm py-4 px-6 rounded-full ml-auto mr-4' type="button">See more</button>
-          <p className='flex justify-end relative md:bottom-8'>from <span>£17.99</span></p>
-          <img className='relative md:bottom-16'src='https://sdcdn.io/mac/gb/mac_sku_SK3709_1x1_2.png?width=1080&height=1080' alt='MAC eyeshadow'></img>
+      <div className='homepagePictures gap-4'>
+        <div className='leftMidPicture '>
+          <h3 className='pl-1 md:text-2xl md:py-2 md:pl-3 lg:text-3xl'>Fast and ecological solutions for you.</h3>
+          <button onClick={BtnDirectShop} className='productsSeeBtn text-xs bg-black text-white px-2 my-2 md:my-6 ml-2 text-sm py-4 px-6 rounded-full  mr-4 lg:text-md' type="button">See more</button>
+          <p className='priceLeft flex justify-end relative mr-2 xl:mr-10'>£17.99</p>
+          <p className='fromWord flex justify-end mr-5 text-xs md:mr-5 lg:mr-3 xl:mr-12'>from</p>
+          <img className='mac relative 'src='https://sdcdn.io/mac/gb/mac_sku_SK3709_1x1_2.png?width=1080&height=1080' alt='MAC eyeshadow'></img>
         </div>
-        <div className='rightMidPicture flex flex-col md:h-5/6 xl:h-5/6'>
+        <div className='rightMidPicture flex flex-col'>
           <div> 
            <h3 className='beautyText text-md mx-2 mr-3 md:text-2xl md:ml-3 md:mt-2 md:w-5/12 lg:text-3xl mr-6'>Beauty has a purpose, <br></br>and the purpose is you.</h3>
-          <button onClick={BtnDirectShop}  className='productsSeeBtn text-xs bg-black text-white px-2 my-2 md:my-6 ml-2 text-sm py-4 px-6 rounded-full ml-auto mr-4' type="button">See more</button>
+          <button onClick={BtnDirectShop}  className='productsSeeBtn text-xs bg-black text-white px-2 my-2 md:my-6 ml-2 text-sm py-4 px-6 rounded-full mr-4 lg:text-md ml-2' type="button">See more</button>
           </div>
           <div className='md:relative bottom-20 '>          
-            <p className='glossierPrice flex justify-end mr-5 text-md md:mr-10 text-2xl lg:mr-24 xl:-32'>£39.99</p>
+            <p className='glossierPrice relative flex justify-end mr-5 text-sm md:mr-10 md:text-2xl lg:mr-24 xl:-32'>£39.99</p>
           <p className='perSet flex justify-end mr-5 text-xs md:mr-10 lg:mr-24 xl:mr-32'>per set</p>
           </div>
-          <img className='glossierSet md:bottom-20 relative lg:bottom-6 xl:bottom-6 ' src={GlossierSet} alt='GlossierSet'></img>
+          <img className='glossierSet relative ' src={GlossierSet} alt='GlossierSet'></img>
         </div>
-      </div>
+      </div> 
       <div>
         <VeganProduct />
       </div>
