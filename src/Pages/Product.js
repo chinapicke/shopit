539c1d1aaea27d1f6e271b23c7da14160a60d008
chinapicke <chinapickering@hotmail.co.uk>
2023-07-18 14:34:53 +0800

    add faviourite text added

--- src/Pages/Product.js
+++ src/Pages/Product.js
@@ -11,6 +11,8 @@ import FooterIcons from '../Components/Banners/FooterIcons'
 import Recommended from '../Components/Recommended'
 import savedHook from '../Hooks/savedHook'
 
+
+
 function Product() {
   // able to use useParams to direct to the page with the id of the product that has been clicked
   const { id } = useParams()
@@ -32,7 +34,6 @@ function Product() {
   const CartState = useContext(AppContext);
   const dispatch = CartState.dispatch;
 
-
   const handleColour = (index) => {
     setClickedColour(prevstate =>
     ({
@@ -41,8 +42,6 @@ function Product() {
     }))
     console.log(setClickedColour)
   }
-  
-
 
   return (
     <>
@@ -63,16 +62,22 @@ function Product() {
             <img src={singleProduct.api_featured_image} alt={singleProduct.product_type}></img>
             </div>
             <div>
-            < button className='mx-1 my-1' value={singleProduct.brand + singleProduct.product_type} onClick={() => { changeIconProduct(); saveDispatch({ type: 'SAVE', saveIt: singleProduct }) }}>
+            < button className='my-1 flex flex-row' value={singleProduct.brand + singleProduct.product_type} onClick={() => { changeIconProduct(); saveDispatch({ type: 'SAVE', saveIt: singleProduct }) }}>
                 {likedIndex ?
                   (
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6" >
-                      <path strokeLinecap="round" strokeLinejoin="round" d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12z" />
+                    <>
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="#b8b4b4" className="w-6 h-6" > 
+                      <path strokeLinecap="round" strokeLinejoin="round" d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12z" /> 
                     </svg>
+                    <h1 className='heartIconCaption'>add to favourites</h1>
+                    </>
                   ):(
-                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="redHeart w-6 h-6">
+                    <>
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="redHeart w-6 h-6"> add to favourites
                       <path d="M11.645 20.91l-.007-.003-.022-.012a15.247 15.247 0 01-.383-.218 25.18 25.18 0 01-4.244-3.17C4.688 15.36 2.25 12.174 2.25 8.25 2.25 5.322 4.714 3 7.688 3A5.5 5.5 0 0112 5.052 5.5 5.5 0 0116.313 3c2.973 0 5.437 2.322 5.437 5.25 0 3.925-2.438 7.111-4.739 9.256a25.175 25.175 0 01-4.244 3.17 15.247 15.247 0 01-.383.219l-.022.012-.007.004-.003.001a.752.752 0 01-.704 0l-.003-.001z" />
                     </svg>
+                    <h1 className='heartIconCaption'>add to favourites</h1>
+                    </>
                   )
 
                 }
