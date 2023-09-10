5a34efd15416ae055e4688c9a826a060abf4bc48
chinapicke <chinapickering@hotmail.co.uk>
2023-09-10 17:12:30 +0100

    styling changes to product page

--- src/Layout/rootLayout.js
+++ src/Layout/rootLayout.js
@@ -81,8 +81,9 @@ export default function RootLayout() {
                 </nav>
                 <nav className='hidden justify-end w-full lg:flex lg:w-auto lg:order-1  md:flex md:w-auto md:order-1' >
                     <ul>
-                        <div className='flex pl-5'>
+                        <div className='flex pl-5 '>
                             <li className='block py-2 pl-3 pr-4 rightSideNavbar' id='findIcon' onClick={() => setCartDrawer(false)}>
+                                
                                 <NavLink to='/shop'>
                                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6">
                                         <path strokeLinecap="round" strokeLinejoin="round" d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z" />
--- src/Pages/Product.js
+++ src/Pages/Product.js
@@ -80,7 +80,7 @@ function Product() {
         </ol>
       </div>
       <div className='productInfo'>
-        {error && <div>{error}</div>}
+        {!error && <div>{error}</div>}
         {isLoading ?
           (<div>Loading...</div>) :
           <div id={singleProduct.id} className='productPageCard relative ml-2 mr-8 md:grid grid-cols-2'>
@@ -95,14 +95,14 @@ function Product() {
                       <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="#b8b4b4" className="w-6 h-6" >
                         <path strokeLinecap="round" strokeLinejoin="round" d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12z" />
                       </svg>
-                      <h1 className='heartIconCaption'>add to favourites</h1>
+                      <h1 className='heartIconCaption mx-2'>add to favourites</h1>
                     </>
                   ) : (
                     <>
                       <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="redHeart w-6 h-6"> add to favourites
                         <path d="M11.645 20.91l-.007-.003-.022-.012a15.247 15.247 0 01-.383-.218 25.18 25.18 0 01-4.244-3.17C4.688 15.36 2.25 12.174 2.25 8.25 2.25 5.322 4.714 3 7.688 3A5.5 5.5 0 0112 5.052 5.5 5.5 0 0116.313 3c2.973 0 5.437 2.322 5.437 5.25 0 3.925-2.438 7.111-4.739 9.256a25.175 25.175 0 01-4.244 3.17 15.247 15.247 0 01-.383.219l-.022.012-.007.004-.003.001a.752.752 0 01-.704 0l-.003-.001z" />
                       </svg>
-                      <h1 className='heartIconCaption'>add to favourites</h1>
+                      <h1 className='heartIconCaption mx-2'>add to favourites</h1>
                     </>
                   )
 
@@ -111,8 +111,7 @@ function Product() {
               <p className='productPageType'>{singleProduct?.product_type ? singleProduct.product_type.charAt(0).toUpperCase() + singleProduct.product_type.slice(1).toLowerCase().split('_').join(' ') : singleProduct.product_type}</p>
               <h1 className='productPageNameBrand '>{singleProduct?.brand ? singleProduct.brand.charAt(0).toUpperCase() + singleProduct.brand.slice(1).toLowerCase() : singleProduct.brand} {singleProduct?.name ? singleProduct.name.charAt(0).toUpperCase() + singleProduct.name.slice(1).toLowerCase() : singleProduct.name}</h1>
               <div className='flex flex-row pb-3 justify-between'>
-                <div className="priceShadow absolute rounded-full mt-4"></div>
-                <h2 className='productPrice mt-4'> £{singleProduct.price === '0.0' || singleProduct.price === null ? '8.50' : Number(singleProduct.price).toFixed(2)}</h2>
+                <h2 className='productPrice mt-4'> <span className='circleShadowProductPg '>£{singleProduct.price === '0.0' || singleProduct.price === null ? '8.50' : Number(singleProduct.price).toFixed(2)}</span></h2>
                 <button className='addBasketProductPage mt-2 mr-2 text-sm text-black px-2 py-3 rounded-full ml-auto mr-4 md:mr-10 md:py-2 lg:mr-12 lg:py-4 lg:px-4' type="button" onClick={() => dispatch({ type: 'ADD', payload: singleProduct })}>add to basket</button>
               </div>
               <h3 className='productPageDescriptionTitle'>Description</h3>
